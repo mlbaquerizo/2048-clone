b2a7ca12538fd91bf7f4adc3d0a2b90a9f46279e
Matthew Baquerizo <mlbaquerizo47@gmail.com>
2016-04-16 18:54:36 -0400

    keybindings will re-render DOM with new numbers

--- js/application.js
+++ js/application.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
 	var game = new Game;
 	game.start();
 	game.toString();
-	tileCount = 0
+	game.bindAll();
+	var tileCount = 0
 	_.each(game.tiles, function(row, i){
 		_.each(row, function(tile){
 			tileCount += 1
@@ -16,6 +17,28 @@ $(document).ready(function() {
 				$('#row4 #tile'+ tileCount).html(tile.value)
 			}
 		})
+	})
+
+	$(this).keyup(function(e){
+		if(e.keyCode == 37 || e.keyCode == 38 || e.keyCode == 39 || e.keyCode == 40){
+		game.bindAll();
+		var tileCount = 0
+		// debugger;
+			_.each(game.tiles, function(row, i){
+				_.each(row, function(tile){
+					tileCount += 1
+					if(i === 0){
+						$('#row1 #tile'+ tileCount).html(tile.value);
+					} else if(i === 1){
+						$('#row2 #tile'+ tileCount).html(tile.value);
+					} else if(i === 2){
+						$('#row3 #tile'+ tileCount).html(tile.value);
+					} else {
+						$('#row4 #tile'+ tileCount).html(tile.value)
+					}
+				})
+			});
+		}
 	})
 
 });
