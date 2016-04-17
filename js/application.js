66ceabcb2ce6c7ac80b4d6e8b9e01ed440d5cf62
Matthew Baquerizo <mlbaquerizo47@gmail.com>
2016-04-17 10:26:51 -0400

    Style tiles with colors

--- js/application.js
+++ js/application.js
@@ -4,15 +4,15 @@ $(document).ready(function() {
 	game.toString();
 	game.bindAll();
 	renderBoard(game);
+	colorizeTiles();
 
 	$(this).keyup(function(e){
 		if(e.keyCode == 37 || e.keyCode == 38 || e.keyCode == 39 || e.keyCode == 40){
-		game.bindAll();
-		// debugger;
+			game.bindAll();
 			renderBoard(game);
+			colorizeTiles();
 		}
 	})
-
 });
 
 function renderBoard(game) {
@@ -31,4 +31,43 @@ function renderBoard(game) {
 			}
 		})
 	})
+}
+
+function colorizeTiles() {
+	var $zeroTiles = $('.tile:contains(0)');
+	$zeroTiles.css('background-color', '#a2a399');
+	$zeroTiles.css('color', '#a2a399');
+	var $twoTiles = $('.tile:contains(2)');
+	$twoTiles.css('background-color', '#bfdad0');
+	$twoTiles.css('color', '#405b50');
+	var $fourTiles = $('.tile:contains(4)');
+	$fourTiles.css('background-color', '#a6cbbd');
+	$fourTiles.css('color', '#405b50');
+	var $eightTiles = $('.tile:contains(8)');
+	$eightTiles.css('background-color', '#49b88d');
+	$eightTiles.css('color', 'white');
+	var $sixteenTiles = $('.tile:contains(16)');
+	$sixteenTiles.css('background-color', '#41a57e');
+	$sixteenTiles.css('color', 'white');
+	var $threeTwoTiles = $('.tile:contains(32)');
+	$threeTwoTiles.css('background-color', '#338062');
+	$threeTwoTiles.css('color', 'white');
+	var $sixFourTiles = $('.tile:contains(64)');
+	$sixFourTiles.css('background-color', '#245c46');
+	$sixFourTiles.css('color', 'white');
+	var $oneTwoEightTiles = $('.tile:contains(128)');
+	$oneTwoEightTiles.css('background-color', '#bf5b81');
+	$oneTwoEightTiles.css('color', 'white');
+	var $twoFiveSixTiles = $('.tile:contains(256)');
+	$twoFiveSixTiles.css('background-color', '#a54167');
+	$twoFiveSixTiles.css('color', 'white');
+	var $fiveTwelveTiles = $('.tile:contains(512)');
+	$fiveTwelveTiles.css('background-color', '#933a5c');
+	$fiveTwelveTiles.css('color', 'white');
+	var $tenTwentyFourTiles = $('.tile:contains(1024)');
+	$tenTwentyFourTiles.css('background-color', '#843452');
+	$tenTwentyFourTiles.css('color', 'white');
+	var $twentyFourEightTiles = $('.tile:contains(2048)');
+	$twentyFourEightTiles.css('background-color', '#3a5c93');
+	$twentyFourEightTiles.css('color', 'white');
 }
