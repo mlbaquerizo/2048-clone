dc3b510302418bef3d3b1e89fc1364374a729388
Matthew Baquerizo <mlbaquerizo47@gmail.com>
2016-04-17 09:22:04 -0400

    Refactor application.js

--- js/application.js
+++ js/application.js
@@ -3,6 +3,19 @@ $(document).ready(function() {
 	game.start();
 	game.toString();
 	game.bindAll();
+	renderBoard(game);
+
+	$(this).keyup(function(e){
+		if(e.keyCode == 37 || e.keyCode == 38 || e.keyCode == 39 || e.keyCode == 40){
+		game.bindAll();
+		// debugger;
+			renderBoard(game);
+		}
+	})
+
+});
+
+function renderBoard(game) {
 	var tileCount = 0
 	_.each(game.tiles, function(row, i){
 		_.each(row, function(tile){
@@ -18,27 +31,4 @@ $(document).ready(function() {
 			}
 		})
 	})
-
-	$(this).keyup(function(e){
-		if(e.keyCode == 37 || e.keyCode == 38 || e.keyCode == 39 || e.keyCode == 40){
-		game.bindAll();
-		var tileCount = 0
-		// debugger;
-			_.each(game.tiles, function(row, i){
-				_.each(row, function(tile){
-					tileCount += 1
-					if(i === 0){
-						$('#row1 #tile'+ tileCount).html(tile.value);
-					} else if(i === 1){
-						$('#row2 #tile'+ tileCount).html(tile.value);
-					} else if(i === 2){
-						$('#row3 #tile'+ tileCount).html(tile.value);
-					} else {
-						$('#row4 #tile'+ tileCount).html(tile.value)
-					}
-				})
-			});
-		}
-	})
-
-});
+}
